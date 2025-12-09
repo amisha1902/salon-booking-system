f7c182687a0e4f232e5fc8a32408700cb90b51a2
Rushi751 <panchalrushikesh2467890@gmail.com>
2025-12-09 16:35:25 +0530

    Path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/customer/Home/HomePage'
+import BookingPage from './pages/customer/BookingForm/BookingPage'
+import ProfilePage from './pages/customer/Profile/ProfilePage'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
@@ -16,8 +16,13 @@ function App() {
           path='/'
             element={<HomePage/>}
           />
-
-
+           <Route path='/book-Appointment'
+           element={<BookingPage/>}
+           />
+           <Route 
+           path='/profile-page'
+           element={<ProfilePage/>}
+           />
         </Routes>
       </div>
       
